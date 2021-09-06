2adf95ecbdd31df591298a9c58bc604e4c0f9f43
Sirine Achour <47246110+SirineAchour@users.noreply.github.com>
2021-09-06 13:37:07 +0100

    trying out one repo and 2 branches

--- bin/update-gh-pages.sh
+++ bin/update-gh-pages.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
-echo "here"
 set -eu
-echo "here 2"
+
 repo_uri="https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_ACTOR}/sirineachour.github.io.git"
 remote_name="origin"
 main_branch="main"
@@ -10,37 +9,37 @@ build_dir="docs"
 
 cd "${GITHUB_WORKSPACE}"
 
-echo "GITHUB_ACTOR"
-echo ${GITHUB_ACTOR}
-echo "GITHUB_WORKSPACE"
-echo ${GITHUB_WORKSPACE}
+
 git config user.name "${GITHUB_ACTOR}"
 git config user.email "${GITHUB_ACTOR}@bots.github.com"
 
-echo "***about to show git status"
-git status
-echo "***about to show git branch"
-git branch
 echo "***about to show git stash"
 git stash
-echo "***about to show git checkout"
-git checkout "$target_branch"
-echo "***about to rebase"
-#git rebase "$remote_name/$main_branch"
-git status
+echo "***done git stashing"
 
 echo "***about to npm i"
 npm i
 echo "about to run build"
 npm run build
 echo "done building"
-git add "$build_dir"
 
-git commit -m "updated GitHub Pages"
+echo "gonna empty out kol chay"
+
+echo "deleting all but docs"
+rm -rfv !(docs|.git)
+echo "done deleting"
+ls -a
+#git add "$build_dir"
+
+#git add "$build_dir"
+
+git add .
+git commit -m "built + clean up"
 if [ $? -ne 0 ]; then
     echo "nothing to commit"
     exit 0
 fi
 
 git remote set-url "$remote_name" "$repo_uri" # includes access token
-git push --force-with-lease "$remote_name" "$target_branch"
+#git push --force-with-lease "$remote_name" "$target_branch"
+git push origin main:gh-pages --force
