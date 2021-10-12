bc000c9ecca08e3753321b761c3c90ec17383132
Sirine Achour <47246110+SirineAchour@users.noreply.github.com>
2021-10-12 01:00:51 +0100

    Update skills.component.ts

--- src/app/components/skills/skills.component.ts
+++ src/app/components/skills/skills.component.ts
@@ -39,7 +39,7 @@ export class SkillsComponent implements OnInit {
       {
         domain:"DevOps",
         skills:[
-          "Cypress", "Docker", "Git", "GitHub Actions", "Kubernetes", "OpenShift", "Puppet", "RabbitMQ","Selenium", "Terraform"
+          "Cypress", "Docker", "Git", "GitHub Actions", "Kubernetes", "OpenShift", "Puppet", "RabbitMQ","Selenium", "Vagrant"
         ]
       },
       {
